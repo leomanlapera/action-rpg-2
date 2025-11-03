c7427eec21829292127223aea585e482391d9010
June Leoman Lapera <leomanlapera@gmail.com>
2025-11-03 22:06:40 +0800

    attacking

--- player/player.gd
+++ player/player.gd
@@ -6,17 +6,32 @@ var input_vector: = Vector2.ZERO
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 
-func _physics_process(delta: float) -> void:
-	input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
+func _physics_process(_delta: float) -> void:
+	var state = playback.get_current_node()
+
+	match state:
+		"MoveState": move_state(_delta)
+		"AttackState": pass
+
+func move_state(_delta: float) -> void:
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
 	if input_vector != Vector2.ZERO:
 		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
-		animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
-		animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
+		update_blend_positions(direction_vector)
 	
+	if Input.is_action_just_pressed("attack"):
+		playback.travel("AttackState")
+
 	if input_vector.x != 0:
 		sprite_2d.flip_h = input_vector.x < 0
-	
+
 	velocity = input_vector * SPEED
 	move_and_slide()
+
+func update_blend_positions(direction_vector: Vector2):
+	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/AttackState/blend_position", direction_vector)
